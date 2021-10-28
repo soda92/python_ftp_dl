e97845e0387a2cfc4d0544f131241ffb0c5207fd
SodaCris <18463922396@163.com>
2021-10-28 16:51:35 +0800

    fin

--- download.py
+++ download.py
@@ -2,6 +2,8 @@
 from ftplib import FTP
 from os import getcwd
 from pathlib import Path
+import time
+import sys
 
 CURR_DIR = Path(getcwd())
 
@@ -18,23 +20,54 @@ def read_config():
         dest_conf = dest_conf.strip()
         if dest_conf != '':
             dest = dest_conf
-        server_list = [line.replace('\n', '').split(',')
-                       for line in content[3:]]
+        server_list = []
+        for line in content[3:]:
+            arr = line.replace('\n', '').split(',')
+            conf = dict()
+            conf['name'] = arr[0].strip()
+            conf['host'] = arr[1].strip()
+            conf['user'] = arr[2].strip()
+            conf['passwd'] = arr[3].strip()
+            server_list.append(conf)
     return dest, server_list
 
-
-# %%
-dest, server_list = read_config()
-name, ip, username, password = server_list[0]
-# %%
-ftp = FTP(source_address=(ip, 22), user=username, passwd=password)
-
 # %%
 
 
-def dl(dest, server):
-    dest = Path(dest).resolve()
+def dl_(dest, ftp):
+    start_time = time.time()
+    ftp.cwd('/home/toybrick/lamp_sample')
+    for directory in ftp.nlst():
+        local_dir = Path.joinpath(dest, server_config['name'], directory)
+        if not local_dir.is_dir():
+            local_dir.mkdir(parents=True)
+        ftp.cwd(directory)
+
+        filenames = ftp.nlst()
+        for i, filename in enumerate(filenames):
+            # print(f"retriving: {i}/{len(filenames)}")
+            local_file = Path.joinpath(local_dir, filename)
+            if local_file.is_file():
+                continue
+            with open(local_file, mode='wb') as f:
+                ftp.retrbinary('RETR ' + filename, f.write)
+        ftp.cwd('..')
+    print("function cost {:.3f} seconds".format(time.time() - start_time))
+    sys.stdout.flush()
+
+
+def dl(dest, server_config):
+    dest = Path(dest)
     dest.mkdir(parents=True, exist_ok=True)
-    for name, ip, username, password in server:
-        with FTP(host=ip, user=username, passwd=password) as ftp:
-            FTP.retrbinary()
+
+    with FTP(host=server_config['host']) as ftp:
+        ftp.login(user=server_config['user'], passwd=server_config['passwd'])
+        dl_(dest=dest, ftp=ftp)
+
+# %%
+
+
+if __name__ == '__main__':
+    dest, server_list = read_config()
+    for server_config in server_list:
+        dl(dest, server_config)
