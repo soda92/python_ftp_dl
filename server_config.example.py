08612f512d2cfe246dd62dbbb134b55423a21915
SodaCris <18463922396@163.com>
2021-11-04 13:43:41 +0800

    add sample comfig

--- server_config.example.py
+++ server_config.example.py
@@ -1,5 +1,8 @@
-server = "192.168.0.134"
-local_name = "test-server"
+# the folder name that will be created on server
+hostname = "test-server"
+local_dir = "/home/toybrick/lamp_sample"
+server_addr = "192.168.0.134"
+
 # 1: skip
 # 2: check-length
 exist_policy = 1
